d739eae61459eaab9650bb4d860ed8db49f8e9a8
Rafael <elgondu@gmail.com>
2020-05-28 18:43:03 -0400

    add JWT

---
+++ src/main/java/com/reddit/backend/security/JwtProvider.java
@@ -0,0 +1,42 @@
+package com.reddit.backend.security;
+
+import com.reddit.backend.exception.SpringRedditException;
+import com.reddit.backend.model.User;
+import io.jsonwebtoken.Jwts;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.*;
+import java.security.cert.CertificateException;
+
+@Service
+public class JwtProvider {
+    private KeyStore keyStore;
+    @PostConstruct
+    public void init() {
+        try {
+            keyStore = KeyStore.getInstance("JKS");
+            InputStream resourceAsStream = getClass().getResourceAsStream("/springblog.jks");
+            keyStore.load(resourceAsStream, "secret".toCharArray());
+        } catch (KeyStoreException | CertificateException | NoSuchAlgorithmException | IOException e) {
+            throw new SpringRedditException("Exception occurred while loading keystore");
+        }
+    }
+    public String generateToken(Authentication authentication) {
+        org.springframework.security.core.userdetails.User principal = (org.springframework.security.core.userdetails.User) authentication.getPrincipal();
+        return Jwts.builder()
+                .setSubject(principal.getUsername())
+                .signWith(getPrivateKey())
+                .compact();
+    }
+    private PrivateKey getPrivateKey() {
+        try {
+            return (PrivateKey) keyStore.getKey("springblog", "secret".toCharArray());
+        } catch (KeyStoreException | NoSuchAlgorithmException | UnrecoverableKeyException e) {
+            throw new SpringRedditException("Exception occured while retrieving public key from keystore");
+        }
+    }
+}
