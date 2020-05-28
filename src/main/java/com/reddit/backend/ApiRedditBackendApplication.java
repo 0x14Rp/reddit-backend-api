3e041546a2650e8e1deed21ca3a5aa3b6ea22e70
Rafael <elgondu@gmail.com>
2020-05-27 23:08:22 -0400

    init project setup

---
+++ src/main/java/com/reddit/backend/ApiRedditBackendApplication.java
@@ -0,0 +1,13 @@
+package com.reddit.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRedditBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRedditBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reddit/backend/ApiRedditBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reddit.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRedditBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
