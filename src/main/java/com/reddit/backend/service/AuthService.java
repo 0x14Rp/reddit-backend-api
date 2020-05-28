3baddaaf40fd1d45be7a2f6df010422a198d4b4d
Rafael <elgondu@gmail.com>
2020-05-28 14:01:31 -0400

    add service to send email verification user register

---
+++ src/main/java/com/reddit/backend/service/AuthService.java
@@ -0,0 +1,61 @@
+package com.reddit.backend.service;
+
+import com.reddit.backend.dto.RegisterRequest;
+import com.reddit.backend.model.NotificationEmail;
+import com.reddit.backend.model.User;
+import com.reddit.backend.model.VerificationToken;
+import com.reddit.backend.repository.UserRepository;
+import com.reddit.backend.repository.VerificationTokenRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+import static com.reddit.backend.util.Constants.ACTIVATION_EMAIL;
+import static java.time.Instant.now;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class AuthService {
+
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final VerificationTokenRepository verificationTokenRepository;
+    private final MailContentBuilder mailContentBuilder;
+    private final MailService mailService;
+
+    @Transactional
+    public void signup(RegisterRequest registerRequest) {
+        User user = new User();
+        user.setUsername(registerRequest.getUsername());
+        user.setEmail(registerRequest.getEmail());
+        user.setPassword(encodePassword(registerRequest.getPassword()));
+        user.setCreated(now());
+        user.setEnabled(false);
+
+        userRepository.save(user);
+        String token = generateVerificationToken(user);
+        String message = mailContentBuilder.build("Thank you for signing up to Spring Reddit, please click on the below url to activate your account : "
+                + ACTIVATION_EMAIL + "/" + token);
+        mailService.sendMail(new NotificationEmail("Please Activate your account", user.getEmail(), message));
+    }
+
+    private String encodePassword(String password) {
+        return passwordEncoder.encode(password);
+    }
+
+
+    private String generateVerificationToken(User user) {
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+        verificationTokenRepository.save(verificationToken);
+        return token;
+    }
+}
---
+++ src/main/java/com/reddit/backend/util/Constants.java
@@ -0,0 +1,9 @@
+package com.reddit.backend.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Constants {
+
+    public static final String ACTIVATION_EMAIL = "http://localhost:8080/api/auth/accountVerification";
+}
