9336ca62b9c19ff4a923f9b9592b7066fceb5a1a
Rafael <elgondu@gmail.com>
2020-05-31 20:20:59 -0400

    refactor post controller

--- src/main/java/com/reddit/backend/controller/PostController.java
+++ src/main/java/com/reddit/backend/controller/PostController.java
@@ -13,13 +13,13 @@ import java.util.List;
 import static org.springframework.http.ResponseEntity.status;
 
 @RestController
-@RequestMapping("/api/post/")
+@RequestMapping("/api/posts/")
 @AllArgsConstructor
 public class PostController {
 
     private final PostService postService;
 
- /*   @PostMapping
+    @PostMapping
     public ResponseEntity<Void> createPost(@RequestBody PostRequest postRequest) {
         postService.save(postRequest);
         return new ResponseEntity<>(HttpStatus.CREATED);
@@ -27,7 +27,7 @@ public class PostController {
 
     @GetMapping
     public ResponseEntity<List<PostResponse>> getAllPosts() {
-        return status(HttpStatus.OK).body(postService.getAllPosts());
+        return status(HttpStatus.OK).body(postService.getAllPost());
     }
 
     @GetMapping("/{id}")
@@ -37,11 +37,11 @@ public class PostController {
 
     @GetMapping("by-subreddit/{id}")
     public ResponseEntity<List<PostResponse>> getPostsBySubreddit(Long id) {
-        return status(HttpStatus.OK).body(postService.getPostsBySubreddit(id));
+        return status(HttpStatus.OK).body(postService.getPostBySubReddit(id));
     }
 
     @GetMapping("by-user/{name}")
     public ResponseEntity<List<PostResponse>> getPostsByUsername(String username) {
-        return status(HttpStatus.OK).body(postService.getPostsByUsername(username));
-    }*/
+        return status(HttpStatus.OK).body(postService.getPostByUserName(username));
+    }
 }
