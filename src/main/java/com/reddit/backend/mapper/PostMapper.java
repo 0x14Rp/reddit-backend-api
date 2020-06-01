2b5454cd640f1a808685184d8422323dc76f8062
Rafael <elgondu@gmail.com>
2020-05-31 20:21:50 -0400

    refactor post mapper

--- src/main/java/com/reddit/backend/mapper/PostMapper.java
+++ src/main/java/com/reddit/backend/mapper/PostMapper.java
@@ -11,10 +11,15 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface PostMapper {
     @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
+    @Mapping(target = "subreddit" , source = "subreddit")
+    @Mapping(target = "user" , source = "user")
     @Mapping(target = "description", source = "postRequest.description")
     Post map(PostRequest postRequest, Subreddit subreddit, User user);
 
     @Mapping(target = "id", source = "postId")
+    @Mapping(target = "postName" , source = "postName")
+    @Mapping(target = "description" , source = "description")
+    @Mapping(target = "url" , source = "url")
     @Mapping(target = "subredditName", source = "subreddit.name")
     @Mapping(target = "userName", source = "user.username")
     PostResponse mapToDto(Post post);
