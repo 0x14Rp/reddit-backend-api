d0422d51d1d8daca8f0b66d5955f6b8f6fb57e2b
Rafael <elgondu@gmail.com>
2020-06-01 23:24:59 -0400

    add vote controller

---
+++ src/main/java/com/reddit/backend/controller/VoteController.java
@@ -0,0 +1,25 @@
+package com.reddit.backend.controller;
+
+import com.reddit.backend.dto.VoteDto;
+import com.reddit.backend.service.VoteService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/votes/")
+@AllArgsConstructor
+public class VoteController {
+
+    private final VoteService voteService;
+
+    @PostMapping
+    public ResponseEntity <Void> vote (@RequestBody VoteDto voteDto) {
+        voteService.vote (voteDto);
+        return new ResponseEntity<>(HttpStatus. OK );
+    }
+}
