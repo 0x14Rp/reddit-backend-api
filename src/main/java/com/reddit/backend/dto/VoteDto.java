7371ed77eaa921dc9751575cb6c961db5ee771fc
Rafael <elgondu@gmail.com>
2020-06-01 23:25:27 -0400

    add voteDto

---
+++ src/main/java/com/reddit/backend/dto/VoteDto.java
@@ -0,0 +1,14 @@
+package com.reddit.backend.dto;
+
+import com.reddit.backend.model.VoteType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VoteDto {
+    private VoteType voteType;
+    private Long postId;
+}
