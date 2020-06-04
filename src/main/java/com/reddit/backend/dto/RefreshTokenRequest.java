cf1a646b77d9f069b9aac9360bcaa51ea4041827
Rafael <elgondu@gmail.com>
2020-06-03 23:27:28 -0400

    add services for refresh token of user

---
+++ src/main/java/com/reddit/backend/dto/RefreshTokenRequest.java
@@ -0,0 +1,18 @@
+package com.reddit.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RefreshTokenRequest {
+
+    @NotBlank
+    private String refreshToken;
+    private String username;
+}
+
---
+++ src/main/java/com/reddit/backend/service/RefreshTokenService.java
@@ -0,0 +1,36 @@
+package com.reddit.backend.service;
+
+import com.reddit.backend.exception.SpringRedditException;
+import com.reddit.backend.model.RefreshToken;
+import com.reddit.backend.repository.RefreshTokenRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+@Transactional
+public class RefreshTokenService {
+
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshToken generateRefreshToken() {
+        RefreshToken refreshToken = new RefreshToken();
+        refreshToken.setToken(UUID.randomUUID().toString());
+        refreshToken.setCreatedDate(Instant.now());
+
+        return refreshTokenRepository.save(refreshToken);
+    }
+
+    void validateRefreshToken(String token) {
+        refreshTokenRepository.findByToken(token)
+                .orElseThrow(() -> new SpringRedditException("Invalid refresh Token"));
+    }
+
+    public void deleteRefreshToken(String token) {
+        refreshTokenRepository.deleteByToken(token);
+    }
+}
