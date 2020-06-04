53993385e7a56f21b9856f8762f5e138c29468c1
Rafael <elgondu@gmail.com>
2020-06-03 23:25:14 -0400

    add properties for class response

--- src/main/java/com/reddit/backend/dto/AuthenticationResponse.java
+++ src/main/java/com/reddit/backend/dto/AuthenticationResponse.java
@@ -1,11 +1,17 @@
 package com.reddit.backend.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+
+import java.time.Instant;
 
 @Data
 @AllArgsConstructor
+@Builder
 public class AuthenticationResponse {
     private String authenticationToken;
     private String username;
+    private Instant expireAt;
+    private String refreshToken;
 }
