6c1753275db516a7a6921885fac0903182cca1a0
R! <elgondu@gmail.com>
2020-06-05 09:50:35 -0400

    add config for swagger documentation

--- src/main/java/com/reddit/backend/ApiRedditBackendApplication.java
+++ src/main/java/com/reddit/backend/ApiRedditBackendApplication.java
@@ -1,12 +1,15 @@
 package com.reddit.backend;
 
 
+import com.reddit.backend.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
+@Import(SwaggerConfiguration.class)
 @SpringBootApplication
 public class ApiRedditBackendApplication {
 
--- src/main/java/com/reddit/backend/config/SecurityConfig.java
+++ src/main/java/com/reddit/backend/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.reddit.backend.security.JwtAuthenticationFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -35,9 +36,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/api/auth/**")
                 .permitAll()
+                .antMatchers(HttpMethod.GET, "/api/subreddit")
+                .permitAll()
+                .antMatchers("/v2/api-docs",
+                        "/configuration/ui",
+                        "/swagger-resources/**",
+                        "/configuration/security",
+                        "/swagger-ui.html",
+                        "/webjars/**")
+                .permitAll()
                 .anyRequest()
                 .authenticated();
-        httpSecurity.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        httpSecurity.addFilterBefore(jwtAuthenticationFilter,
+                UsernamePasswordAuthenticationFilter.class);
     }
 
     @Autowired
