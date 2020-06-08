6452854d043df218d49d0a0f5f7ca098149aa782
Rafael <elgondu@gmail.com>
2020-06-07 22:46:20 -0400

    fix bug user assigned post

--- src/main/java/com/reddit/backend/mapper/PostMapper.java
+++ src/main/java/com/reddit/backend/mapper/PostMapper.java
@@ -31,6 +31,7 @@ public abstract class PostMapper {
     @Mapping(target = "description", source = "postRequest.description")
     @Mapping(target = "subreddit", source = "subreddit")
     @Mapping(target = "voteCount", constant = "0")
+    @Mapping(target = "user", source = "user")
     public abstract Post map(PostRequest postRequest, Subreddit subreddit, User user);
 
     @Mapping(target = "id", source = "postId")
