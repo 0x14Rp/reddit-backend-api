06406d8c26ba39dab03a0735d8955b3c9d4cd581
Rafael <elgondu@gmail.com>
2020-06-20 23:30:44 -0400

    add test authServiceClass

---
+++ src/test/java/com/reddit/backend/service/AuthServiceTest.java
@@ -0,0 +1,18 @@
+package com.reddit.backend.service;
+
+import com.reddit.backend.model.User;
+import com.reddit.backend.repository.VerificationTokenRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class AuthServiceTest {
+
+
+
+}
