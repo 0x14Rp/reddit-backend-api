4f9fc41f2269654a24950a6c4574a31eea75596c
Rafael <elgondu@gmail.com>
2020-06-27 23:39:36 -0400

    comment

--- src/main/java/com/reddit/backend/util/Constants.java
+++ src/main/java/com/reddit/backend/util/Constants.java
@@ -5,5 +5,7 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class Constants {
 
+
+    ///CONSTANST
     public static final String ACTIVATION_EMAIL = "http://localhost:8080/api/auth/accountVerification";
 }
